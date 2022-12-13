fba0fd4861c38193de04f8965670707428d06bf6
Eman <emanamir546@gmail.com>
2022-12-13 18:12:51 +0500

    Initial commit for p5.js starter template code.

---
+++ game.js
@@ -0,0 +1,17 @@
+console.log("This is a p5.js project. ");
+
+// All images and fonts will loaded here : 
+function preload() {
+    
+}
+
+// Our setup happens here : 
+function setup(){
+    createCanvas(innerWidth, innerHeight);
+}
+
+// Our loop that draws everything again and again : 
+function draw(){
+    background("black");
+    background("black");
+}
