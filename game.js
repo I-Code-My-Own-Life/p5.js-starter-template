ce4a5816e8ba01558add2bcb17ad0a3a7ad58a00
Eman <emanamir546@gmail.com>
2022-12-13 18:44:13 +0500

    Second commit for p5.js starter template. I have added some fonts to the template project.

--- game.js
+++ game.js
@@ -1,17 +1,27 @@
 console.log("This is a p5.js project. ");
 
+// Our canvas element : 
+let canvas = {
+    width:innerWidth,
+    height:innerHeight
+}
 // All images and fonts will loaded here : 
+let font1;
 function preload() {
-    
+    font1 = loadFont("Fonts/font22.ttf");
 }
 
 // Our setup happens here : 
 function setup(){
-    createCanvas(innerWidth, innerHeight);
+    createCanvas(canvas.width, canvas.height);
 }
 
 // Our loop that draws everything again and again : 
 function draw(){
     background("black");
-    background("black");
+    textSize(50);
+    textFont(font1);
+    fill("white");
+    text('P5.js Template', canvas.width / 2, canvas.height / 2);
+    textAlign(CENTER);
 }
