539119a02d75b5e4ce1519a11e838ff8a213fb68
Eman <emanamir546@gmail.com>
2024-01-05 10:29:56 +0500

    Initial commit

--- game.js
+++ game.js
@@ -1,27 +1,94 @@
-console.log("This is a p5.js project. ");
-
-// Our canvas element : 
+// console.log("This is a p5.js project. ");
+// // Our canvas object :
 let canvas = {
-    width:innerWidth,
-    height:innerHeight
-}
-// All images and fonts will loaded here : 
-let font1;
+  width: innerWidth,
+  height: innerHeight,
+};
+
+let grid = [];
+let gridSize = 100;
+let row, col;
+let currentColor;
+
+let plantImg;
 function preload() {
-    font1 = loadFont("Fonts/font22.ttf");
+  plantImg = loadImage("image.png");
 }
 
-// Our setup happens here : 
-function setup(){
-    createCanvas(canvas.width, canvas.height);
+let plants = [];
+class Plant {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  draw() {
+    image(plantImg, this.x, this.y, this.width, this.height);
+  }
 }
 
-// Our loop that draws everything again and again : 
-function draw(){
-    background("black");
-    textSize(50);
-    textFont(font1);
-    fill("white");
-    text('P5.js Template', canvas.width / 2, canvas.height / 2);
-    textAlign(CENTER);
-}
+
+function setup() {
+    let canvasWidth = min(windowWidth, 1920);
+    let canvasHeight = min(windowHeight, 1080);
+  
+    createCanvas(canvasWidth, canvasHeight + 50);
+  
+    row = floor(height / gridSize);
+    col = floor(width / gridSize);
+    for (let i = 0; i < row; i++) {
+      grid[i] = [];
+      for (let j = 0; j < col; j++) {
+        if ((i + j) % 2 === 0) {
+          grid[i][j] = "#08fc50";
+        } else {
+          grid[i][j] = "#21c452";
+        }
+      }
+    }
+  }
+  
+  function draw() {
+    noStroke();
+  
+    // Draw grid
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        fill(grid[i][j]);
+        rect(j * gridSize, i * gridSize, gridSize, gridSize);
+      }
+    }
+  
+    // Draw all plants
+    for (let plant of plants) {
+      plant.draw();
+    }
+  }
+  
+  function mousePressed() {
+    let i = floor(mouseY / gridSize);
+    let j = floor(mouseX / gridSize);
+  
+    if (
+      i >= 0 &&
+      i < row &&
+      j >= 0 &&
+      j < col &&
+      (grid[i][j] === "#08fc50" || grid[i][j] === "#21c452")
+    ) {
+      // Place plant on the specific grid
+      let newPlant = new Plant(j * gridSize + 25, i * gridSize + 25, 40, 60);
+      plants.push(newPlant);
+    }
+  }
+  
+  function windowResized() {
+    let canvasWidth = min(windowWidth, 1920);
+    let canvasHeight = min(windowHeight, 1080);
+  
+    resizeCanvas(canvasWidth, canvasHeight);
+  
+    row = floor(height / gridSize);
+    col = floor(width / gridSize);
+  }
